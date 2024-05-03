66fae02444083f7362e1b78fda40b0d729c5cbc1
NoDioxide <nodioxide@gmail.com>
2024-05-03 16:44:54 +0300

    Update about.php
    Наработки для вкладки About us

--- about.php
+++ about.php
@@ -37,6 +37,15 @@
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
                 </div>
             </div>
+
+            <div class="blocks2">
+                <div class="block2">
+                    <img src="images/game2.svg" alt="">
+                    <span>Lorem ipsum</span>   
+                    <h2>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>
+                </div>
+            </div>
         </div>
     </div>
 
